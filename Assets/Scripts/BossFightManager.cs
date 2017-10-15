00d3554d4e90e472d3b30ca0577480ac31ad0edd
Sterling McLeod <sterlingmcleod9@gmail.com>
2017-10-15 09:34:14 -0400

    Move player from Game scene to Boss scene, handle setting new variables and disabling isocontrol component to prevent movement

--- Assets/Scripts/BossFightManager.cs
+++ Assets/Scripts/BossFightManager.cs
@@ -27,6 +27,17 @@ public class BossFightManager : MonoBehaviour {
 		dropDown = GameObject.Find ("/GUI/BossFightActions").GetComponent<Dropdown> ();
 		dropDown.onValueChanged.AddListener(DropdownValueChanged);
 
+		// Set player object
+		player = GameObject.Find ("Player").GetComponent<Player> ();
+		Vector3 playerPos = new Vector3 (25f, 0.5f, 15f);
+		player.transform.position = playerPos;
+
+		// Set player action list for player object
+		player.actionList = GameObject.Find ("PlayerActionList").GetComponent<PlayerActionList> ();
+
+		// Disable player movement
+		player.GetComponent<IsoCharControl> ().enabled = false;
+
 		init = false;
 	}
 
