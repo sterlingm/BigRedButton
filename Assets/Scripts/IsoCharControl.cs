8b21d505ca1f7ba3fc10775e20b16870ea024b5b
Sterling McLeod <sterlingmcleod9@gmail.com>
2017-12-04 22:17:44 -0500

    Decided on prefabs to use for the environment, changed player cube size, adjusted isocharcontrol. TODO: Rotate camera to look towards the player, currently it is facing away from the player

--- Assets/Scripts/IsoCharControl.cs
+++ Assets/Scripts/IsoCharControl.cs
@@ -51,10 +51,12 @@ public class IsoCharControl : MonoBehaviour {
 
 		// Move vector is the position change in heading direction based on speed*time
 		Vector3 moveVec = heading * (moveSpeed * Time.deltaTime);
-
+		Debug.Log(String.Format("position: {0} moveVec: {1}", transform.position.ToString(), moveVec.ToString()));
 		// Set new heading and add position change to transform.position
 		transform.forward = heading;
 		transform.position += moveVec;
+		Debug.Log(String.Format("After, position: {0}", transform.position.ToString()));
+				
 
 		// Update the camera's position so that the player stays in view
 		CenterCameraOnPlayer ();
@@ -63,7 +65,7 @@ public class IsoCharControl : MonoBehaviour {
 	void CenterCameraOnPlayer()
 	{
 		Vector3 v_player = transform.position;
-		v_player.y = 20;
+		v_player.y = 10;
 
 		Camera.main.transform.position = v_player;
 	}
